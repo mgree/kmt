59dc034cfcfca159b474dd495358a5c045ed2231
Michael Greenberg <michael.greenberg@pomona.edu>
2020-03-26 08:43:07 -0700

    pulling out slow tests...

--- src/tests.ml
+++ src/tests.ml
@@ -6,25 +6,23 @@ open Product
 open Automata
 open Decide
 
-module T = ANSITerminal
-
-exception Violation of string * string
-
-type result = 
-  | Success 
-  | Failure of string * string
+type mode = Normalization | Automata
 
 module type TESTER =
   functor (T : THEORY) ->
   sig
     type t = string
 
+    val mode : mode
+           
     val pp : t Fmt.t
 
     val equal : t -> t -> bool
     
-    val assert_equivalent : string -> string -> string -> unit Alcotest.test_case
-    val assert_not_equivalent : string -> string -> string -> unit Alcotest.test_case
+    val assert_equivalent : ?speed:Alcotest.speed_level ->
+                            string -> string -> string -> unit Alcotest.test_case
+    val assert_not_equivalent : ?speed:Alcotest.speed_level ->
+                                string -> string -> string -> unit Alcotest.test_case
   end
 
 module AutomataTester(T : THEORY) = struct
@@ -33,6 +31,8 @@ module AutomataTester(T : THEORY) = struct
 
   type t = string
 
+  let mode = Automata
+         
   let pp fmt x = Fmt.pf fmt "%s" x
          
   let equal s1 s2 =          
@@ -44,10 +44,10 @@ module AutomataTester(T : THEORY) = struct
 
   let equivalent : string Alcotest.testable = Alcotest.testable pp equal
 
-  let assert_equivalent name l r =
-    Alcotest.test_case name `Quick (fun () -> Alcotest.(check equivalent) "equivalent" l r)
-  let assert_not_equivalent name l r =
-    Alcotest.test_case name `Quick (fun () -> Alcotest.(check (Alcotest.neg equivalent)) "inequivalent" l r)
+  let assert_equivalent ?speed:(speed=`Quick) name l r =
+    Alcotest.test_case name speed (fun () -> Alcotest.(check equivalent) "equivalent" l r)
+  let assert_not_equivalent ?speed:(speed=`Quick) name l r =
+    Alcotest.test_case name speed (fun () -> Alcotest.(check (Alcotest.neg equivalent)) "inequivalent" l r)
 end
 
 module NormalizationTester(T : THEORY) = struct
@@ -55,6 +55,8 @@ module NormalizationTester(T : THEORY) = struct
   module D = Decide(T)
 
   type t = string
+
+  let mode = Normalization
 
   let pp fmt x = Fmt.pf fmt "%s" x
 
@@ -65,19 +67,11 @@ module NormalizationTester(T : THEORY) = struct
     
   let equivalent : string Alcotest.testable = Alcotest.testable pp equal
 
-  let assert_equivalent name l r =
-    Alcotest.test_case name `Quick (fun () -> Alcotest.(check equivalent) "equivalent" l r)
-  let assert_not_equivalent name l r =
-    Alcotest.test_case name `Quick (fun () -> Alcotest.(check (Alcotest.neg equivalent)) "inequivalent" l r)
+  let assert_equivalent ?speed:(speed=`Quick) name l r =
+    Alcotest.test_case name speed (fun () -> Alcotest.(check equivalent) "equivalent" l r)
+  let assert_not_equivalent ?speed:(speed=`Quick) name l r =
+    Alcotest.test_case name speed (fun () -> Alcotest.(check (Alcotest.neg equivalent)) "inequivalent" l r)
 end
-
-let (%) f g = fun x -> g (f x)
-
-let run test = 
-  try 
-    test ();
-    Success 
-  with Violation(s1,s2) -> Failure(s1,s2) 
 
 (* Unit tests *)
 module TestAddition (T : TESTER) = struct
@@ -194,53 +188,53 @@ module TestBoolean (T : TESTER) = struct
   open TB
 
   let tests =
-    [ assert_equivalent "boolean assign eq"
+    [ assert_equivalent "assign eq"
          "set(x,T); x=T"
          "set(x,T)";
-      assert_not_equivalent "boolean assign neq"
+      assert_not_equivalent "assign neq"
          "set(x,T); x=F"
          "set(x,T)";
-      assert_equivalent "boolean assign eq 2"
+      assert_equivalent "assign eq 2"
          "set(x,T); set(x,F); x=F"
          "set(x,T); set(x,F)";
-      assert_equivalent  "boolean parity loop"
+      assert_equivalent  "parity loop"
         "x=F; ( (x=T; set(x,F) + x=F; set(x,T));(x=T; set(x,F) + x=F; set(x,T)) )*"
         "     ( (x=T; set(x,F) + x=F; set(x,T));(x=T; set(x,F) + x=F; set(x,T)) )*; x=F";
-      assert_not_equivalent "boolean parity loop 2"
+      assert_not_equivalent "parity loop 2"
         "x=F; ( (x=T; set(x,F) + x=F; set(x,T));(x=T; set(x,F) + x=F; set(x,T)) )*"
         "     ( (x=T; set(x,F) + x=F; set(x,T));(x=T; set(x,F) + x=F; set(x,T)) )*; x=T";
-      assert_equivalent "boolean finiteness"
+      assert_equivalent "finiteness"
          "x=F + x=T"
          "true";
-      assert_equivalent "boolean associativity"
+      assert_equivalent "associativity"
          "(x=F + y=F) + z=F"
          "x=F + (y=F + z=F)";
-      assert_equivalent "boolean multiple vars"
+      assert_equivalent "multiple vars"
          "(x=T; set(y,T) + x=F; set(y,F)); (x=T;y=T + x=F;y=F)"
          "(x=T; set(y,T) + x=F; set(y,F))";
-      assert_not_equivalent "boolean multiple vars 2"
+      assert_not_equivalent "multiple vars 2"
          "(x=T; set(y,T) + x=F; set(y,F)); (x=T;y=F + x=F;y=F)"
          "(x=T; set(y,T) + x=F; set(y,F))";
-      assert_equivalent "boolean unrolling"
+      assert_equivalent "unrolling"
          "set(x,T)*; x=T"
          "x=T + set(x,T); set(x,T)*; x=T";
-      assert_equivalent "boolean tree ordering"
-        "set(w,F); set(x,T); set(y,F); set(z,F); ((w=T + x=T + y=T + z=T); set(a,T) + (not (w=T + x=T + y=T + z=T)); set(a,F))"
-        "set(w,F); set(x,T); set(y,F); set(z,F); (((w=T + x=T) + (y=T + z=T)); set(a,T) + (not ((w=T + x=T) + (y=T + z=T))); set(a,F))";
       assert_equivalent "kat p* identity"
         "(a=T)*"
-        "(a=T;a=T)* + a=T;(a=T;a=T)*"
-    ]
+        "(a=T;a=T)* + a=T;(a=T;a=T)*"      
+    ] @ (if mode = Automata
+         then []
+         else [ assert_equivalent "tree ordering"
+                  "set(w,F); set(x,T); set(y,F); set(z,F); ((w=T + x=T + y=T + z=T); set(a,T) + (not (w=T + x=T + y=T + z=T)); set(a,F))"
+                  "set(w,F); set(x,T); set(y,F); set(z,F); (((w=T + x=T) + (y=T + z=T)); set(a,T) + (not ((w=T + x=T) + (y=T + z=T))); set(a,F))"
+           ])
 end
 
 module TestProduct (T : TESTER) = struct
   module TP = T(Product(Addition)(Boolean))
   open TP
 
-  (* assert_equivalent "is this even right?"
-      "y<1; (a=F + a=T; inc(y,1)); (b=F + b=T; inc(y,1)); (c=F + c=T; inc(y,1)); y>2"
-      "a=T; b=T; c=T; inc(y,1); inc(y,1); inc(y,1)" *)
-
+  let slow = if mode = Automata then `Slow else `Quick
+     
   let tests =
     [ assert_equivalent "product parsing"
         "set(x,T); x=T; inc(y,1)"
@@ -251,12 +245,15 @@ module TestProduct (T : TESTER) = struct
       assert_equivalent  "product population count"
         "y<1; (a=F + a=T; inc(y,1)); y > 0"
         "y<1; a=T; inc(y,1)";
-      assert_not_equivalent  "product population count 2"
+      assert_not_equivalent ~speed:slow "product population count 2"
         "y<1; (a=F + a=T; inc(y,1))"
         "a=T; inc(y,1)";
-      assert_equivalent "product population count 3"
+      assert_equivalent ~speed:slow "product population count 3"
         "y<1; (true + a=T; inc(y,1)); (true + b=T; inc(y,1)); (true + c=T; inc(y,1)); y>2"      
-        "y<1; a=T; b=T; c=T; inc(y,1); inc(y,1); inc(y,1)"
+        "y<1; a=T; b=T; c=T; inc(y,1); inc(y,1); inc(y,1)";
+      assert_equivalent ~speed:slow "product population count 3 (variant)"
+      "y<1; (a=F + a=T; inc(y,1)); (b=F + b=T; inc(y,1)); (c=F + c=T; inc(y,1)); y>2"
+      "y<1; a=T; b=T; c=T; inc(y,1); inc(y,1); inc(y,1)"
     ]
                                             
 end
